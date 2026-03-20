9c8901cf3f9a81e88d5165f6719177bdd282cf03
Kamalesh <kamaleshbabu622@gmail.com>
2026-03-20 19:35:00 +0530

    Add external port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import dotenv from "dotenv";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(methodOverride('_method'));
 
